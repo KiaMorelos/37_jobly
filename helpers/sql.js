cf5f893c54eed981583c8b0f500f79ecee06b548
KiaMorelos <kiana.morelos@gmail.com>
2022-09-07 22:50:11 -0600

    added documentation for the sqlForPartialUpdate function in helpers/sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,19 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** sqlForPartialUpdate: Use to help create a valid sql query for a partial update.
+   *
+   * sqlForPartial update is expecting data in object format: dataToUpdate = {firstName: "Chester", lastName: "Bennington" }
+   * Object.keys creates an array of the properties of the data: ["firstName", "lastName"]
+   * if no data was given return a BadRequestError
+   * 
+   * Otherwise map keys array (and get its index plus 1) and turn into a SQL query. While also using 
+   * the jsToSql that was given to make the JS variables into valid SQL {firstName: "first_name"} 
+   * creating a query that looks something like: `first_name = $1` with multiples items separted by ','
+   * 
+   * values: Object.values turns the values of the dataToUpdate into an array: [ "Chester", "Bennington"]
+   * 
+   * Returns { setCols: "first_name = $1 , last_name = $2 ", values: [ "Chester", "Bennington" ]}
+**/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
